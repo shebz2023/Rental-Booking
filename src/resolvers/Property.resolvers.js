d8aca731008153e734fb7a220376fd4015567773
shebz2023 <shebelleke@gmail.com>
2025-02-18 11:07:52 +0200

    [Add]Property resolvers

--- src/resolvers/Property.resolvers.js
+++ src/resolvers/Property.resolvers.js
@@ -1,5 +1,36 @@
 import { propertyService } from "../services/Property.service.js";
 export const propertyResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getProperty: (_, args) => {
+      const { id } = args;
+      const payload = propertyService.getProperty(id);
+      return payload;
+    },
+
+    getProperties: async () => {
+      const payload = propertyService.getProperties();
+      return payload;
+    },
+  },
+  Mutation: {
+    createOneProperty: (_, args, context) => {
+      const { property } = args.input;
+      const payload = propertyService.createOneProperty(property, context.user);
+      return payload;
+    },
+    updateOneProperty: async (_, args, context) => {
+      const { id, update } = args.input;
+      const payload = await propertyService.updateOneProperty(
+        id,
+        update,
+        context.user
+      );
+      return payload;
+    },
+    deleteOneProperty: async (_, args, context) => {
+      const { id } = args.input;
+      const payload = await propertyService.deleteOneProperty(id);
+      return payload;
+    },
+  },
 };
