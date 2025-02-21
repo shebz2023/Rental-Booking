722ba3bd98bfcbe80aec1e5143ce15c8796b3c3e
shebz2023 <shebelleke@gmail.com>
2025-02-21 14:24:52 +0200

    ch:CORS access

--- src/server.js
+++ src/server.js
@@ -1,15 +1,31 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema.js";
-import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { mergeResolvers } from "@graphql-tools/merge";
 import { userResolvers } from "./resolvers/User.resolvers.js";
 import { propertyResolver } from "./resolvers/Property.resolvers.js";
 import { bookingResolver } from "./resolvers/Booking.resolver.js";
 import { authorizeUser } from "./middlewares/token.js";
 
 const port = process.env.PORT || 8080;
-const resolvers = mergeResolvers([userResolvers, propertyResolver ,bookingResolver]);
+const resolvers = mergeResolvers([
+  userResolvers,
+  propertyResolver,
+  bookingResolver,
+]);
 
-new ApolloServer({ resolvers, typeDefs, context: authorizeUser }).listen(
-  { port },
-  () => console.log(`Server ready at: http://localhost:${port}🚀`)
-);
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  context: authorizeUser,
+});
+
+server
+  .listen({
+    port,
+    cors: {
+      origin: "*",
+    },
+  })
+  .then(({ url }) => {
+    console.log(`Server ready at: ${url} 🚀`);
+  });
